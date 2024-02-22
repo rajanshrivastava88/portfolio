aa99b32b96ed4344cdd05121b749061bd8d1dfa7
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-02-23 04:21:36 +0530

    Update app.js

---
+++ portfolio/app.js
@@ -0,0 +1,21 @@
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+
+
+var typed = new Typed(".text", {
+    strings: ["Youtuber", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
