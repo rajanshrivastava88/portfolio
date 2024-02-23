d0d87a2a7b749348c212b3ddfcd56250f0390dda
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-02-23 20:39:54 +0530

    Update app.js

--- portfolio/app.js
+++ portfolio/app.js
@@ -4,12 +4,15 @@
             document.querySelector(".active-btn").classList.remove("active-btn");
             this.classList.add("active-btn");
             document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
         })
     });
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
     })
 })();
+
+
 
 
 var typed = new Typed(".text", {
